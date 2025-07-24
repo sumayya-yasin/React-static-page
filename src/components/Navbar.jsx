35f74a1867dddcc7218eabf8193712d07dcd26ba
Sumayya-Yasin <sumayyabintyasin@gmail.com>
2025-07-24 16:15:31 +0500

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,11 @@
+import reactLogo from "../images/react-logo.png";
+
 export default function Navbar() {
   return (
     <header className="header">
       <nav className="header__nav">
-        <img
-          className="header__logo"
-          src="src\images\react-logo.png"
-          alt="react logo"
-        />
-        <span className="header__text"> ReactFacts </span>
+        <img className="header__logo" src={reactLogo} alt="React logo" />
+        <span className="header__text">ReactFacts</span>
       </nav>
     </header>
   );
