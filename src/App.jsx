280a17678436d8a29548a94b3a574b5ff9e5460e
sumayya-yasin <sumayyabintyasin@gmail.com>
2025-08-04 15:13:17 +0500

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./index.css";
 import Navbar from "./components/Navbar.jsx";
 import Main from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
-    <>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Navbar />
-      <Main />
-    </>
+      <div style={{ flexGrow: 1 }}>
+        <Main />
+      </div>      
+      <Footer />
+    </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="custom-footer">
+        © {new Date().getFullYear()} Sumayya · ReactFacts · All rights reserved.
+    </footer>
+  );
+}
